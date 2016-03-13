db264870c94106855a80c1f106e0f1c58ea9de32
Chris Blackburn <burnicus8@gmail.com>
2016-03-13 11:55:44 -0400

    add Origin header to all requests

--- main.js
+++ main.js
@@ -17,4 +17,13 @@ app.on('ready', () => {
   }
 
   mainWindow.on('closed', () => mainWindow = null)
+
+  // Since this is an Electron app, the browser will not be adding an Origin
+  // header to any requests. So we add it here manually to all requests from the
+  // app.
+  const request = mainWindow.webContents.session.webRequest
+  request.onBeforeSendHeaders(function(details, callback) {
+    details.requestHeaders['Origin'] = 'report-generator';
+    callback({cancel: false, requestHeaders: details.requestHeaders});
+  })
 })
